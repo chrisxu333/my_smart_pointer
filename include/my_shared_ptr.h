fb96b0a4d737181af71926b66037600bf996f31c
chrisxu333 <xu.3296@osu.edu>
2022-08-31 05:24:51 +0800

    multithreaded shared_ptr

--- include/my_shared_ptr.h
+++ include/my_shared_ptr.h
@@ -1,40 +1,37 @@
 #ifndef MY_SHARED_PTR_H_
 #define MY_SHARED_PTR_H_
 
+#include <mutex>
+
 template <typename T>
 class my_shared_ptr {
  public:
   // constructors
-  my_shared_ptr(T* other) {
-    ptr_ = other;
-    refcnt_ = new int(1);
-  }
+  my_shared_ptr(T* other)
+      : ptr_(other), refcnt_(new int(1)), refcnt_lock_(new std::mutex) {}
   // copy constructor
-  my_shared_ptr(const my_shared_ptr& other) {
-    ptr_ = other.ptr_;
-    refcnt_ = other.refcnt_;
-    (*refcnt_)++;
+  my_shared_ptr(const my_shared_ptr<T>& other)
+      : ptr_(other.ptr_),
+        refcnt_(other.refcnt_),
+        refcnt_lock_(other.refcnt_lock_) {
+    inc_ref_count();
   }
   // destructor
-  ~my_shared_ptr() {
-    (*refcnt_)--;
-    if (*refcnt_ == 0) {
-      delete ptr_;
-    }
-  }
+  ~my_shared_ptr() { release(); }
   // Replaces the managed object with the one managed by other.
-  my_shared_ptr& operator=(const my_shared_ptr& other) noexcept {
-    // deal with old ptr
-    this->~my_shared_ptr();
-    // assign to new pointer
-    *refcnt_ = other.refcnt_;
-    ptr_ = other.ptr_;
+  my_shared_ptr<T>& operator=(const my_shared_ptr<T>& other) noexcept {
+    if (ptr_ != other.ptr_) {
+      release();
+      ptr_ = other.ptr_;
+      refcnt_ = other.refcnt_;
+      refcnt_lock_ = other.refcnt_lock_;
+      inc_ref_count();
+    }
     return *this;
   }
 
   // observer
   T& operator*() const noexcept { return *ptr_; }
-
   T* operator->() const noexcept { return ptr_; }
 
   // Return the stored pointer.
@@ -44,17 +41,41 @@ class my_shared_ptr {
     if (ptr != ptr_) {
       if (ptr_) delete ptr_;
       ptr_ = ptr;
-      *refcnt_ = 1;
+      reset_ref_count();
     }
   }
 
   long use_count() const noexcept { return *refcnt_; }
-
   bool unique() const noexcept { return use_count() == 1; }
 
  private:
+  std::mutex* refcnt_lock_;
   int* refcnt_;
   T* ptr_;
+
+  void inc_ref_count() {
+    refcnt_lock_->lock();
+    (*refcnt_)++;
+    refcnt_lock_->unlock();
+  }
+
+  void reset_ref_count() {
+    refcnt_lock_->lock();
+    *refcnt_ = 1;
+    refcnt_lock_->unlock();
+  }
+
+  void release() {
+    refcnt_lock_->lock();
+    (*refcnt_)--;
+    if (*refcnt_ == 0) {
+      delete ptr_;
+      delete refcnt_;
+      refcnt_lock_->unlock();
+      delete refcnt_lock_;
+    }
+    refcnt_lock_->unlock();
+  }
 };
 
 #endif
